b6ab9d0f71576e46e6860501f67105447a55c7fc
bannakonz <bannakon@gmail.com>
2023-10-14 15:49:51 +0700

    add place detail screen

--- lib/widget/places_list.dart
+++ lib/widget/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screen/places_detail.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/places.dart';
@@ -29,6 +30,15 @@ class PlacesList extends StatelessWidget {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => PlaceDetailScreen(
+                place: places[index],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
